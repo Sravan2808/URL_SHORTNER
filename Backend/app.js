695cd5f13a611e4595514a3fb2eedc78beb9b7da
Sravan Surya <vsravansurya@gmail.com>
2025-08-02 23:50:29 +0530

    Code Modularised

--- Backend/app.js
+++ Backend/app.js
@@ -3,35 +3,17 @@ import dotenv from "dotenv";
 dotenv.config("./.env");
 import connectDB from "./src/config/mongo_config.js";
 import { nanoid } from "nanoid";
-import urlSchema from "./src/models/shorturl.model.js";
+import urlSchema from "./src/models/short_url.model.js"
+import short_url from "./src/routes/short_url.route.js";
+import { redirectFromShortUrl } from "./src/controller/short_url.controller.js";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/api/create", (req, res) => {
-  const {url} = req.body;
-  const shortUrl = nanoid(7);
-  const newUrl = new urlSchema({
-    full_url:url,
-    short_url:shortUrl,
-  })
-  newUrl.save();
-  res.json({message:"URL created successfully",shortUrl:shortUrl})
-});
+app.use("/api/create",short_url);
 
-app.get("/:id",async(req,res)=>{
-    const {id} = req.params
-    const url = await urlSchema.findOne({
-        short_url:id
-    })
-    if(url){
-        res.redirect(url.full_url);
-    }
-    else{
-        res.status(404).json({message:"URL not found"})
-    }
-})
+app.use("/:id", redirectFromShortUrl);
 
 app.listen(3000, () => {
   connectDB();
