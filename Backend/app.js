0e1b605a00c0bef8a95b4b86c4deb0c0f85e611c
Sravan Surya <vsravansurya@gmail.com>
2025-08-02 13:14:57 +0530

    Created a Basic Backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,39 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config("./.env");
+import connectDB from "./src/config/mongo_config.js";
+import { nanoid } from "nanoid";
+import urlSchema from "./src/models/shorturl.model.js";
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/api/create", (req, res) => {
+  const {url} = req.body;
+  const shortUrl = nanoid(7);
+  const newUrl = new urlSchema({
+    full_url:url,
+    short_url:shortUrl,
+  })
+  newUrl.save();
+  res.json({message:"URL created successfully",shortUrl:shortUrl})
+});
+
+app.get("/:id",async(req,res)=>{
+    const {id} = req.params
+    const url = await urlSchema.findOne({
+        short_url:id
+    })
+    if(url){
+        res.redirect(url.full_url);
+    }
+    else{
+        res.status(404).json({message:"URL not found"})
+    }
 })
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-    
-})
+app.listen(3000, () => {
+  connectDB();
+  console.log("Server is running on  http://localhost:3000");
+});
---
+++ Backend/src/config/mongo_config.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        if (!process.env.MONGO_URL) {
+            throw new Error("MONGO_URI environment variable is not defined");
+        }
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
